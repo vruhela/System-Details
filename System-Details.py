38d8ca30592434e08337d1f2d1e8a382c4ce2fba
Vedansh Ruhela <86541021+vruhela@users.noreply.github.com>
2021-07-29 12:28:45 +0530

    Initial Commit

---
+++ System-Details.py
@@ -0,0 +1,17 @@
+import os
+import platform
+
+print("OS name is", os.name)
+print("CPU count is", os.cpu_count())
+print("System is", platform.system())
+print("Release is", platform.release())
+print("Architecture is", platform.architecture())
+print("Processor is",platform.processor())
+print("Machine is", platform.machine())
+print("System is", platform.system())
+print("Platform is", platform.platform())
+print("Node is", platform.node())
+
+
+
+
